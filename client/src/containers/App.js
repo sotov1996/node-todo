c4b8ce59f4ed0ffd831ac3a88323463b453bd59a
sotov1996 <ilyapoliana@gmail.com>
2020-10-22 16:59:15 +0300

    put

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -6,6 +6,8 @@ const App = () => {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
+  const [user, setUser] = useState({});
+  const [action, setAction] = useState(false);
 
   useEffect(() => {
     fetch('/api/muggers')
@@ -29,7 +31,6 @@ const App = () => {
   const onDelete = (id) => {
     const idx = items.findIndex((el) => el._id === id);
     console.log(idx);
-    setItems([...items.slice(0,idx),...items.slice(idx+1)]);
 
     fetch('/api/muggers' + "/" + id, {
       method: 'DELETE'
@@ -38,7 +39,39 @@ const App = () => {
     }).catch(err => {
       console.error(err)
     }); 
+    setItems([...items.slice(0,idx),...items.slice(idx+1)]);
   }
+
+  const getNewUser = (e) => {
+    e.preventDefault();
+    fetch('/api/muggers' + "/" + user._id, {
+      method: 'PATCH',
+      body: JSON.stringify({
+       items
+      })
+    }).then((response) => {
+      response.json().then((response) => {
+        console.log(response);
+      })
+    }).catch(err => {
+      console.error(err)
+    })
+    setItems({...items, user});
+  }
+
+  const onChangeUser = (id) => {
+    const idx = items.findIndex((el) => el._id === id);
+    setUser(items[idx]);
+    setAction(!action)
+  }
+
+  const onChangeName = (e) => {
+    setUser({name: e.target.value})
+  }
+  /*const onChangeAttribute = (e) => {
+    const users = e.target.value;
+    setUser({});
+  }*/
     
   if (error) {
     return <div>Ошибка: {error.message}</div>;
@@ -57,11 +90,18 @@ const App = () => {
                 <p>{`Status is: ${el.status}`}</p>
               </li>
               <button onClick={() => onDelete(el._id)}>del</button>
+              <button onClick={() => onChangeUser(el._id)}>edit</button>
             </div>
         )
       })}
     </ul>
     <PostForm newItem={newTtem}/>
+    {action ? <div>
+                 <input  type="sumbit"onChange={onChangeName} value={user.name}/>
+                 <input value={user.age}/>
+                 <input value={user.status}/>
+                 <input type='button' onclick={getNewUser} value="start"/>
+              </div> : <h1>sdf</h1>}
     </div>
   )
 }
